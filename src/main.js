00eeb0c03e1a0dcac4d9ea86b66092b23b034bfc
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-01-31 22:42:15 +0700

    Basic note

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
+	target: document.body, // It means that all code we write would be injected to <body>...</body>
+	props: { // The way we set props for "App" component
 		name: 'world'
 	}
 });
