1abf9b4a1dceb1fc2e4756ad1a8aed5814c8d087
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-01 01:11:03 +0700

    Shorthand store

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,24 +1,10 @@
 <script>
-    import { onDestroy, onMount } from "svelte";
     import { fade, scale } from "svelte/transition";
     import FeedbackItem from "./FeedbackItem.svelte"
     import { FeedbackStore } from "../stores";
-    
-    let feedback = [];
-    
-    const unsubscribe = FeedbackStore.subscribe((data) => feedback = data);
-
-    onMount(() => {
-        console.log("mounted")
-    })
-
-    onDestroy(() => {
-        unsubscribe();
-    })
 </script>
 
-
-{#each feedback as item (item.id)}
+{#each $FeedbackStore as item (item.id)}
     <div in:scale out:fade="{{ duration: 500 }}">
         <FeedbackItem item={item} on:delete-feedback/>
     </div>
