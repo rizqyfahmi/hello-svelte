9dea7c885ae8dbef8adf65d95ffc6fd9f3a93848
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-02-12 09:57:05 +0700

    Store subscribe and unsubscribe

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -1,5 +1,15 @@
 <script>
-    export let count = 0
+    import { onDestroy } from "svelte";
+    import { count } from "../stores/store";
+    
+    let localCount = 0
+    const unsubscribe = count.subscribe((currentCount) => {
+        localCount = currentCount;
+    })
+
+    onDestroy(() => {
+        unsubscribe()
+    })
 </script>
-<h3>Counter: {count}</h3>
+<h3>Counter: {localCount}</h3>
 <style></style>
